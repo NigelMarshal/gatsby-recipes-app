6ed88fe4bc693b5f9adaa30df18db32803211606
NigelMarshal <nigelmarshal@gmail.com>
2021-11-22 20:31:24 +0400

    feat(Page-styles): Set up Home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,32 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
     <Layout>
-      <div>
-        <h2>About Page</h2>
-      </div>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Lorem, ipsum.</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              maiores?
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Salt Bae"
+            className="about-img"
+            placeholder="tracedSVG"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+
 export default function Home() {
   return (
-    <div>
-      <Layout>
-        <h1>Header</h1>
-      </Layout>
-    </div>
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="Baking Background"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Recipes</h1>
+              <h4>Things and Stuff</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
   )
 }
