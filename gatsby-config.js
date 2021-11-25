8ac25d515000a4825830d7fdfb0b279faebb45bd
NigelMarshal <nigelmarshal@gmail.com>
2021-11-25 12:17:46 +0400

    feat(App): Set up connection to contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,35 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Recipes!",
+    description: "Recipe App built with Gatsby",
+    author: "Nigel Marshal",
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `rg24qurik5jo`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
   ],
 }
