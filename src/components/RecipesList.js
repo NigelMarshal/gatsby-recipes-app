1c3d5302af3c3eaed076c775728ab6c26e85fc3c
NigelMarshal <nigelmarshal@gmail.com>
2021-11-25 17:18:51 +0400

    feat(Recipe template): Set up template page and slugs

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import slugify from "slugify"
 
 const RecipesList = ({ recipes = [] }) => {
   return (
@@ -8,8 +9,9 @@ const RecipesList = ({ recipes = [] }) => {
       {recipes.map(recipe => {
         const { id, title, image, prepTime, cookTime } = recipe
         const pathToImage = getImage(image)
+        const slug = slugify(title, { lower: true })
         return (
-          <Link key={id} to={`/${title}`} className="recipe">
+          <Link key={id} to={`/${slug}`} className="recipe">
             <GatsbyImage
               image={pathToImage}
               className="recipe-img"
---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const RecipeTemplate = props => {
+  return (
+    <div>
+      <h2>{props.params.title}</h2>
+    </div>
+  )
+}
+
+export default RecipeTemplate
